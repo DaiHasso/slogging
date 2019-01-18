cf88c9a382f8171471cba4fb60799f7e3e33f4fa
Rohan Cribbs <rohan.cribbs@thoughtforger.com>
2019-01-17 20:34:00 -0800

    Add ability to add a functional attribute instead of just a static one.

--- extra_parameter.go
+++ extra_parameter.go
@@ -6,9 +6,34 @@ import (
 
 type ExtraParameter func(LogInstance) error
 
+// AddExtraAttributes adds all items in a key, value map that will append:
+//   key: value
+// to the log for each item in the map.
 func AddExtraAttributes(attributes map[string]interface{}) ExtraParameter {
 	return func(log LogInstance) error {
 		for key, val := range attributes {
+			log.With(key, val)
+		}
+
+		return nil
+	}
+}
+
+// AddExtraAttributeFuncs adds all items in a key, value (function) map that
+// will append:
+//   key: value()
+// to the log for each item in the map.
+func AddExtraAttributeFuncs(
+	attributeFuncs map[string]func() (interface{}, error),
+) ExtraParameter {
+	return func(log LogInstance) error {
+		for key, val := range attributeFuncs {
+			val, err := val()
+			if err != nil {
+				return errors.Wrap(
+					err, "Error while evaluating attribute for log.",
+				)
+			}
 			log.With(key, val)
 		}
 
